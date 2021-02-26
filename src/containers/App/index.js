7afe744536fb41040b3556dec4626169a04b03e0
abhishek.kumar <abhishek.kumar@hsv.digital>
2021-02-26 18:16:04 +0530

    did changes in index.js

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -5,13 +5,43 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './style.scss';
 import { useLocation, useHistory } from 'react-router-dom';
 
+class ErrorBoundary extends React.Component {
+     constructor(props) {
+       super(props);
+       this.state = { error: null, errorInfo: null };
+     }
+     componentDidCatch(error, errorInfo) {
+       this.setState({
+         error: error,
+         errorInfo: errorInfo,
+       });
+     }
+     render() {
+       if (this.state.errorInfo) {
+         return (
+           <div>
+             <h2>Something went wrong.</h2>
+             <details style={{ whiteSpace: "pre-wrap" }}>
+               {this.state.error && this.state.error.toString()}
+               <br />
+               {this.state.errorInfo.componentStack}
+             </details>
+           </div>
+         );
+       }
+       return this.props.children;
+     }
+   }
+
 const App = (props) => {
      const history = useHistory();
      const location = useLocation();
      return (
           <div className='main_app_container'>
-               <HeaderNav/>
-               <AppRoutes {...props} loaction={location} history={history}/>
+               <ErrorBoundary>
+                    <HeaderNav/>
+                    <AppRoutes {...props} loaction={location} history={history}/>
+               </ErrorBoundary>
           </div>
      )
 }
